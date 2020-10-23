2ac16d0b0fefdfef7c128785430ea44d284de305
Essam Dawood <essam_d@hotmail.com>
2020-10-23 14:27:19 +0200

    created the challengeme and experience pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,10 @@
 class PagesController < ApplicationController
   def home
   end
+  
+  def experience
+  end
+
+  def challengeme
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  get 'experience', to: 'pages#experience'
+  get 'challengeme', to: 'pages#challengeme'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
